f0d46f9409ed60727db01c5dcaa62f512d93f230
Aung Yin <aungyin19@gmail.com>
2021-05-04 22:29:50 +0900

    Product page: Extract common CSS class

--- pages/product.vue
+++ pages/product.vue
@@ -2,25 +2,25 @@
   <main class="">
 
     <!-- HERO -->
-    <section class="hero mt-6 px-2 md:px-10 max-w-screen-lg xl:max-w-screen-xl mx-auto overflow-hidden">
-      <div class="flex flex-col space-y-10 md:flex-row-reverse">
-        <div class="w-full md:w-3/5">
-          <img src="~/assets/img/product.jpg" alt="Product">
+    <section class="hero mt-6 section-container">
+      <div class="hero-container">
+        <div class="hero-img">
+          <img class="mx-auto" src="~/assets/img/product.jpg" alt="Product">
         </div>
-        <div class="w-full h-full md:w-2/5 text-center md:text-left">
-          <h1 class=" font-light text-2xl md:text-3xl lg:text-4xl tracking-widest">小学校受験サービス</h1>
-          <p class=" mt-4 font-thin tracking-widest">豊富な問題集でいつでも簡単に<br class="hidden md:block lg:hidden">受験できる。</p>
-          <p class=" mt-8 md:mt-14">小学生向けの受験サービスです。</p>
-          <a class="mt-4 comm-btn--blue inline-block" href="http://ansurs.com/" target="_blank" rel="noopener noreferrer">今すぐアクセス</a>
+        <div class="hero-content">
+          <h1 class="hero-content__header">小学校受験サービス</h1>
+          <p class="hero-content__text">豊富な問題集でいつでも簡単に<br class="hero-content__br">受験できる。</p>
+          <p class="mt-8 md:mt-12">小学生向けの受験サービスです。</p>
+          <a class="hero-content__link comm-btn--blue" href="http://ansurs.com/" target="_blank" rel="noopener noreferrer">今すぐアクセス</a>
         </div>
       </div>
     </section>
 
     <!-- 目的 -->
-    <section class="aim mt-16 py-16 md:py-20 border-gray-100 border-t border-b bg-gray-100">
-      <div class="px-2 md:px-10 max-w-screen-lg xl:max-w-screen-xl mx-auto">
-        <h2 class=" font-light text-2xl md:text-4xl tracking-widest">小学校受験サービス</h2>
-        <p class="text-sm md:text-base mt-2">
+    <section class="aim mt-16 py-16 md:py-20 bg-gray-100">
+      <div class="section-container">
+        <h2 class="aim-header">小学校受験サービス</h2>
+        <p class="aim-text">
           本サービスは、お勉強や小学校受験向けのコンテンツを提供するサービスとして、練習図形問題16種類、問題数は12,000問以上あります。お勉強や公立・私立小学校お受験をする子供たちはもちろん、ご家族にどんどん情報発信してまいります。<br>
           試験のためだけではなく、子供たちに知識量を増やすこともでき、脳全体を活性化させる事もできます。
         </p>
@@ -29,37 +29,35 @@
 
     <!-- ガイド -->
     <section class="guide py-16 md:py-20">
-      <div class="px-2 md:px-10 max-w-screen-lg xl:max-w-screen-xl mx-auto">
-        <div class="px-2 flex flex-col md:flex-row md:space-x-10">
-          <div class=" w-full md:w-1/2 text-center md:text-left">
-            <div>
-              <img class="w-20 mx-auto md:mx-0" src="~/assets/svg/paper.svg" alt="Paper">
-              <h4 class="mt-6">豊富な問題集</h4>
-            </div>
-            <p class="mt-8">
-              図形問題を含む16種類、問題数は12,000問以上あります。
-            </p>
+      <div class="section-container guide-container">
+        <div class="guide-item">
+          <div>
+            <img class="guide-item__image" src="~/assets/svg/paper.svg" alt="Paper">
+            <h4 class="mt-6">豊富な問題集</h4>
           </div>
-          <div class=" w-full md:w-1/2 mt-10 md:mt-0 text-center md:text-left">
-            <div>
-              <img class="w-20 mx-auto md:mx-0" src="~/assets/svg/printer.svg" alt="Printer">
-              <h4 class="mt-6">問題集を簡単にプラント</h4>
-            </div>
-            <p class="mt-8">
-              インターネットとプリンターさえ用意すれば、いつでもどこでもプリントで勉強できます。
-            </p>
+          <p class="mt-8">
+            図形問題を含む16種類、問題数は12,000問以上あります。
+          </p>
+        </div>
+        <div class="guide-item">
+          <div>
+            <img class="guide-item__image" src="~/assets/svg/printer.svg" alt="Printer">
+            <h4 class="mt-6">問題集を簡単にプラント</h4>
           </div>
+          <p class="mt-8">
+            インターネットとプリンターさえ用意すれば、いつでもどこでもプリントで勉強できます。
+          </p>
         </div>
       </div>
     </section>
 
     <!-- お試し -->
     <section class="trial bg-gray-100 py-6">
-      <div class="flex flex-col md:flex-row justify-center items-center space-x-10">
-        <div class="flex justify-center items-center space-x-4">
-          <img class="w-10 md:w-16" src="~/assets/svg/stars.svg" alt="Stars">
-          <p class=" tracking-wider">無料問題<a class=" underline leading-loose hover:text-blue-medium" href="http://ansurs.com/ItemList/Menu1.aspx" target="_blank" rel="noopener noreferrer">サンプルをみる</a></p>
-        </div>
+      <div class="trial-container">
+        <img class="trial-img" src="~/assets/svg/stars.svg" alt="Stars">
+        <p class=" tracking-wider">
+          無料問題<a class="trial-link" href="http://ansurs.com/ItemList/Menu1.aspx" target="_blank" rel="noopener noreferrer">サンプルをみる</a>
+        </p>
       </div>
     </section>
 
@@ -67,6 +65,63 @@
 </template>
 
 <style scoped>
+
+  .section-container {
+    @apply px-2 md:px-10 max-w-screen-lg xl:max-w-screen-xl mx-auto;
+  }
+
+
+  .hero-container {
+    @apply flex flex-col space-y-10 md:flex-row-reverse;
+  }
+  .hero-img {
+    @apply w-full md:w-3/5;
+  }
+  .hero-content {
+    @apply w-full h-full md:w-2/5 text-center md:text-left;
+  }
+  .hero-content__header {
+    @apply font-light text-2xl md:text-3xl lg:text-4xl tracking-widest;
+  }
+  .hero-content__text {
+    @apply mt-4 font-thin tracking-widest;
+  }
+  .hero-content__br {
+    @apply hidden md:block lg:hidden;
+  }
+  .hero-content__link {
+    @apply mt-4 inline-block;
+  }
+
+
+  .aim-header {
+    @apply font-light text-2xl md:text-4xl tracking-widest;
+  }
+  .aim-text {
+    @apply text-sm md:text-base mt-2;
+  }      
+
+
+  .guide-container {
+    @apply flex flex-col space-y-10 md:flex-row md:space-x-10 md:space-y-0;
+  }
+  .guide-item {
+    @apply w-full md:w-1/2 text-center md:text-left;
+  }
+  .guide-item__image {
+    @apply w-20 mx-auto md:mx-0;
+  }
+
+
+  .trial-container {
+    @apply flex flex-row justify-center items-center space-x-2;
+  }
+  .trial-img {
+    @apply w-10 md:w-16;
+  }
+  .trial-link {
+    @apply underline leading-loose hover:text-blue-medium;
+  }
 
 </style>
 
